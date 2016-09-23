314414816ee1ec3f245c300290061544aa26eed0
Alexandr Alakin <alakinalexandr@gmail.com>
2016-09-23 12:05:35 +0600

    Bytes can be sent by linux and processed by arduino

--- linux_to_arduino.ino
+++ linux_to_arduino.ino
@@ -3,27 +3,59 @@
 #define NUMPIXELS 3
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
 int delayval = 500; // delay for half a second
-int incomingByte;
-String message;
+int stage;
+uint8_t mesType,mesLen,i;
+//uint8_t inData[];
 
 void setup() {
   Serial.begin(9600);
   pixels.begin();
   pixels.show();
+  stage = 0;  //0 - type should be received, 1 - length, 2 - values
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    //message = Serial.readString();
-    incomingByte = Serial.read();
-    if ( incomingByte == '1' ) {
-      showWeather(0, pixels.Color(50,0,0), delayval);
-    } else if ( incomingByte == '2' ) {
-      showWeather(0, pixels.Color(0,50,0), delayval);
+  if (Serial.available()) {
+    if (stage == 0) {
+      Serial.println("");
+      mesType = Serial.read();
+      stage = 1;
+      Serial.print("Stage: 0->1, mesType: ");Serial.println(mesType);
+    } else if (stage == 1) { 
+      mesLen = Serial.read();
+      stage = 2;
+      Serial.print("Stage: 1->2, mesLen: ");Serial.println(mesLen);
+    } else if (stage == 2) {
+      uint8_t inData[mesLen];
+      i = 0;
+      while (Serial.available() and (i<mesLen)) {
+        inData[i] = Serial.read();
+        Serial.print("Stage: 2, inData[");Serial.print(i);Serial.print("]: ");Serial.println(inData[i]);
+        i++;
+      }
+      stage = 0;
+      Serial.print("Stage: ");Serial.println(stage);
+      if (i == mesLen) {
+        showWeather(inData[0], pixels.Color(inData[1],inData[2],inData[3]), delayval);
+      } else {
+        Serial.println("Transaction is bad");
+      }
     }
-    Serial.print("I received: ");
-    Serial.println(incomingByte);
+    
+    
+    
+    //message = Serial.readString();
+    //incomingByte = Serial.read();
+    //showWeather(0, pixels.Color(incomingByte,0,0), delayval);
+//    if ( incomingByte == 0xFF ) {
+//      showWeather(0, pixels.Color(50,0,0), delayval);
+//    } else if ( incomingByte == 0xEE ) {
+//      showWeather(0, pixels.Color(0,50,0), delayval);
+//    }
+    //Serial.print("I received: ");
+    //Serial.print(incomingByte, HEX);
   }
+  //ard.flushInput();
   //showWeather(0, pixels.Color(50,0,0), delayval);
   //showWeather(0, pixels.Color(0,50,0), delayval);
   //showWeather(0, pixels.Color(0,0,50), delayval);
@@ -38,5 +70,4 @@ void showWeather(int pix, uint32_t c, uint16_t wait) {
   pixels.show();
   delay(wait);
 }
-
 
--- linux_to_arduino.py
+++ linux_to_arduino.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 import serial, time
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-print("Waiting for serial initialization")
-time.sleep(2)
-print("Done")
+ard = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
 
-ser.write('121')
+#TODO Check if arduino ready by bidirectional communication:
+# e.g. send 0xFF, until 0xFF is received
+#TODO Check if extra bytes is processed fine
+time.sleep(2)
+
+#ard.write(bytes([0, 4]))
+#ard.write(bytes([40]))
+#ard.write(bytearray([0,4,2,0,255,255]));
+ard.write(bytearray([0,4,2,0,255,255,0,4,2]));
+time.sleep(1);
+ard.write(bytearray([0,4,2,0,255,255,0,4,2]));
+#print (str(ard.read()))
